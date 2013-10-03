363def9deed9f165586e313bcb8a8a489765363c
Zenujow <castillo_luis@hotmail.com>
2013-10-02 17:22:49 -0700

    Prueba2

---
+++ SAREI.Presentacion/src/administrarAgenda/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package administrarAgenda;
+
+/**
+ *
+ * @author Luis Castillo
+ */
+public class NewClass {
+    
+}
